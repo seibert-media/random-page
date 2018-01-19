b3e49b09abffc5a88a973effba2fd7004e254f02
ppasler <ppasler@seibert-media.net>
2018-01-19 11:43:46 +0100

    small changes in package json, fixed PR comments

--- src/main/java/net/seibertmedia/confluence/randompage/macro/RandomPageMacro.java
+++ src/main/java/net/seibertmedia/confluence/randompage/macro/RandomPageMacro.java
@@ -1,5 +1,6 @@
 package net.seibertmedia.confluence.randompage.macro;
 
+import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
 import static com.atlassian.confluence.renderer.radeox.macros.MacroUtils.defaultVelocityContext;
 import static com.atlassian.confluence.util.velocity.VelocityUtils.getRenderedTemplate;
 
@@ -11,47 +12,34 @@ import org.slf4j.LoggerFactory;
 import com.atlassian.confluence.content.render.xhtml.ConversionContext;
 import com.atlassian.confluence.macro.Macro;
 import com.atlassian.confluence.macro.MacroExecutionException;
-import com.atlassian.renderer.RenderContext;
-import com.atlassian.renderer.v2.RenderMode;
-import com.atlassian.renderer.v2.macro.BaseMacro;
-import com.atlassian.renderer.v2.macro.MacroException;
 
-public class RandomPageMacro extends BaseMacro implements Macro {
+public class RandomPageMacro implements Macro {
 
 	private static final Logger log = LoggerFactory.getLogger(RandomPageMacro.class);
 
 	private static final String MACRO_TEMPLATE = "/macro/random-page.vm";
 
-	public String execute(final Map<String, String> parameters, final String body, final ConversionContext context) throws MacroExecutionException {
-		Map<String, Object> velocityContext = defaultVelocityContext();
-		velocityContext.putAll(parameters);
+	private static final String DEFAULT_LIMIT = "1";
+	private static final String LIMIT_KEY = "limit";
 
+	@Override
+	public String execute(final Map<String, String> parameters, final String body, final ConversionContext context) throws MacroExecutionException {
+		log.error("execute macro with params: {}", parameters);
+
+		Map<String, Object> velocityContext = defaultVelocityContext();
+		velocityContext.put(LIMIT_KEY, defaultIfNull(parameters.get(LIMIT_KEY), DEFAULT_LIMIT));
+
+		log.error("execute macro with params: {}", velocityContext);
 		return getRenderedTemplate(MACRO_TEMPLATE, velocityContext);
 	}
 
-	public String execute(final Map parameters, final String body, final RenderContext renderContext) throws MacroException {
-		try {
-			return execute(parameters, body, (ConversionContext) null);
-		} catch (final MacroExecutionException e) {
-			log.warn("Failed to execute macro", e);
-			throw new MacroException(e);
-		}
-	}
-
+	@Override
 	public BodyType getBodyType() {
 		return BodyType.NONE;
 	}
 
+	@Override
 	public OutputType getOutputType() {
 		return OutputType.BLOCK;
 	}
-
-	public RenderMode getBodyRenderMode() {
-		return RenderMode.NO_RENDER;
-	}
-
-	public boolean hasBody() {
-		return false;
-	}
-
 }
